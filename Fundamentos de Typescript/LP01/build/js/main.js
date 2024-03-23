5e30e05a6493ecc28f1066570164bd98ddd3a70f
Brenda Moura <brendamoura.bcdm@gmail.com>
2024-03-23 15:28:07 -0400

    LP01: Map adicionado

--- Fundamentos de Typescript/LP01/build/js/main.js
+++ Fundamentos de Typescript/LP01/build/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 const input = document.querySelector("input");
 const list = document.getElementById("list");
-const activities = [];
+const activities = new Map();
 const TIME_FORMAT = {
     day: "2-digit",
     month: "2-digit",
@@ -11,22 +11,25 @@ const TIME_FORMAT = {
     timeZone: "America/Manaus",
     hour12: false,
 };
-input === null || input === void 0 ? void 0 : input.addEventListener("keypress", addItem);
-function addItem(event) {
-    if (event.keyCode !== 13 || !input || !list) {
+input.addEventListener("keypress", (e) => {
+    if (e.key != "Enter") {
         return;
     }
     const date = new Date();
     const datetime = date.toLocaleString("pt-BR", TIME_FORMAT);
-    activities.push([input.value, `Criado em ${datetime}`]);
+    const newTask = [input.value, `Criado em ${datetime}`];
+    const index = activities.size;
+    activities.set(index, newTask);
     input.value = "";
-    showItems();
-}
+    addItem(newTask, index);
+});
 const deleteItem = (index) => {
-    activities.splice(index, 1);
-    showItems();
+    const item = list.querySelector(`#item${index}`);
+    list.removeChild(item);
+    activities.delete(index);
 };
 const editItem = (index) => {
+    var _a, _b, _c, _d;
     const newTitle = window.prompt("Digite o novo título");
     if (!newTitle || !newTitle.trim()) {
         alert("Por favor, digite um valor válido!");
@@ -34,45 +37,50 @@ const editItem = (index) => {
     }
     const date = new Date();
     const datetime = date.toLocaleString("pt-BR", TIME_FORMAT);
-    activities[index][0] = newTitle;
-    activities[index][1] = `Editado em ${datetime}`;
-    showItems();
+    activities.set(index, [newTitle, `Editado em ${datetime}`]);
+    const item = list.querySelector(`#item${index}`);
+    item.querySelector("h2").textContent = (_b = (_a = activities.get(index)) === null || _a === void 0 ? void 0 : _a[0]) !== null && _b !== void 0 ? _b : "";
+    item.querySelector("p").textContent = (_d = (_c = activities.get(index)) === null || _c === void 0 ? void 0 : _c[1]) !== null && _d !== void 0 ? _d : "";
 };
+const addItem = (activity, index) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const content = document.createElement("div");
+    content.classList.add("content");
+    const title = document.createElement("h2");
+    title.textContent = activity[0];
+    const datetime = document.createElement("p");
+    datetime.textContent = activity[1];
+    const action = document.createElement("div");
+    action.classList.add("action");
+    const btnEdit = document.createElement("input");
+    btnEdit.setAttribute("type", "button");
+    btnEdit.value = "Editar";
+    btnEdit.addEventListener("click", () => {
+        editItem(index);
+    });
+    const btnDelete = document.createElement("input");
+    btnDelete.setAttribute("type", "button");
+    btnDelete.value = "Apagar";
+    btnDelete.addEventListener("click", () => {
+        deleteItem(index);
+    });
+    content.appendChild(title);
+    content.appendChild(datetime);
+    action.appendChild(btnEdit);
+    action.appendChild(btnDelete);
+    card.appendChild(content);
+    card.appendChild(action);
+    card.id = `item${index}`;
+    list.appendChild(card);
+};
+// If there are items in activities
 const showItems = () => {
-    if (!list || !activities) {
+    if (!activities) {
         return;
     }
-    list.innerHTML = "";
-    activities.map((activity, index) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const content = document.createElement("div");
-        content.classList.add("content");
-        const title = document.createElement("h2");
-        title.textContent = activity[0];
-        const datetime = document.createElement("p");
-        datetime.textContent = activity[1];
-        const action = document.createElement("div");
-        action.classList.add("action");
-        const btnEdit = document.createElement("input");
-        btnEdit.setAttribute("type", "button");
-        btnEdit.value = "Editar";
-        btnEdit.addEventListener("click", () => {
-            editItem(index);
-        });
-        const btnDelete = document.createElement("input");
-        btnDelete.setAttribute("type", "button");
-        btnDelete.value = "Apagar";
-        btnDelete.addEventListener("click", () => {
-            deleteItem(index);
-        });
-        content.appendChild(title);
-        content.appendChild(datetime);
-        action.appendChild(btnEdit);
-        action.appendChild(btnDelete);
-        card.appendChild(content);
-        card.appendChild(action);
-        list.appendChild(card);
+    activities.forEach((activity, index) => {
+        addItem(activity, index);
     });
 };
 showItems();
--- Fundamentos de Typescript/LP01/src/main.ts
+++ Fundamentos de Typescript/LP01/src/main.ts
@@ -1,7 +1,7 @@
-const input: HTMLInputElement | null = document.querySelector("input");
-const list: HTMLElement | null = document.getElementById("list");
+const input: HTMLInputElement = document.querySelector("input")!;
+const list: HTMLElement = document.getElementById("list")!;
 
-const activities: [string, string][] = [];
+const activities = new Map<number, [string, string]>();
 
 const TIME_FORMAT: Intl.DateTimeFormatOptions = {
   day: "2-digit",
@@ -13,10 +13,8 @@ const TIME_FORMAT: Intl.DateTimeFormatOptions = {
   hour12: false,
 };
 
-input?.addEventListener("keypress", addItem);
-
-function addItem(event: { keyCode: number }): void {
-  if (event.keyCode !== 13 || !input || !list) {
+input.addEventListener("keypress", (e) => {
+  if (e.key != "Enter") {
     return;
   }
 
@@ -24,15 +22,22 @@ function addItem(event: { keyCode: number }): void {
 
   const datetime: string = date.toLocaleString("pt-BR", TIME_FORMAT);
 
-  activities.push([input.value, `Criado em ${datetime}`]);
+  const newTask: [string, string] = [input.value, `Criado em ${datetime}`];
+
+  const index: number = activities.size;
+
+  activities.set(index, newTask);
 
   input.value = "";
-  showItems();
-}
+
+  addItem(newTask, index);
+});
 
 const deleteItem = (index: number): void => {
-  activities.splice(index, 1);
-  showItems();
+  const item: Element = list.querySelector(`#item${index}`)!;
+
+  list.removeChild(item);
+  activities.delete(index);
 };
 
 const editItem = (index: number): void => {
@@ -46,58 +51,64 @@ const editItem = (index: number): void => {
 
   const datetime: string = date.toLocaleString("pt-BR", TIME_FORMAT);
 
-  activities[index][0] = newTitle;
-  activities[index][1] = `Editado em ${datetime}`;
-  showItems();
+  activities.set(index, [newTitle, `Editado em ${datetime}`]);
+
+  const item: Element = list.querySelector(`#item${index}`)!;
+  item.querySelector("h2")!.textContent = activities.get(index)?.[0] ?? "";
+  item.querySelector("p")!.textContent = activities.get(index)?.[1] ?? "";
 };
 
+const addItem = (activity: [string, string], index: number): void => {
+  const card: HTMLDivElement = document.createElement("div");
+  card.classList.add("card");
+
+  const content: HTMLDivElement = document.createElement("div");
+  content.classList.add("content");
+
+  const title: HTMLHeadingElement = document.createElement("h2");
+  title.textContent = activity[0];
+
+  const datetime: HTMLParagraphElement = document.createElement("p");
+  datetime.textContent = activity[1];
+
+  const action: HTMLDivElement = document.createElement("div");
+  action.classList.add("action");
+
+  const btnEdit: HTMLInputElement = document.createElement("input");
+  btnEdit.setAttribute("type", "button");
+  btnEdit.value = "Editar";
+  btnEdit.addEventListener("click", () => {
+    editItem(index);
+  });
+
+  const btnDelete: HTMLInputElement = document.createElement("input");
+  btnDelete.setAttribute("type", "button");
+  btnDelete.value = "Apagar";
+  btnDelete.addEventListener("click", () => {
+    deleteItem(index);
+  });
+
+  content.appendChild(title);
+  content.appendChild(datetime);
+
+  action.appendChild(btnEdit);
+  action.appendChild(btnDelete);
+
+  card.appendChild(content);
+  card.appendChild(action);
+  card.id = `item${index}`;
+
+  list.appendChild(card);
+};
+
+// If there are items in activities
 const showItems = (): void => {
-  if (!list || !activities) {
+  if (!activities) {
     return;
   }
 
-  list.innerHTML = "";
-
-  activities.map((activity, index) => {
-    const card: HTMLDivElement = document.createElement("div");
-    card.classList.add("card");
-
-    const content: HTMLDivElement = document.createElement("div");
-    content.classList.add("content");
-
-    const title: HTMLHeadingElement = document.createElement("h2");
-    title.textContent = activity[0];
-
-    const datetime: HTMLParagraphElement = document.createElement("p");
-    datetime.textContent = activity[1];
-
-    const action: HTMLDivElement = document.createElement("div");
-    action.classList.add("action");
-
-    const btnEdit: HTMLInputElement = document.createElement("input");
-    btnEdit.setAttribute("type", "button");
-    btnEdit.value = "Editar";
-    btnEdit.addEventListener("click", () => {
-      editItem(index);
-    });
-
-    const btnDelete: HTMLInputElement = document.createElement("input");
-    btnDelete.setAttribute("type", "button");
-    btnDelete.value = "Apagar";
-    btnDelete.addEventListener("click", () => {
-      deleteItem(index);
-    });
-
-    content.appendChild(title);
-    content.appendChild(datetime);
-
-    action.appendChild(btnEdit);
-    action.appendChild(btnDelete);
-
-    card.appendChild(content);
-    card.appendChild(action);
-
-    list.appendChild(card);
+  activities.forEach((activity: [string, string], index: number) => {
+    addItem(activity, index);
   });
 };
 
