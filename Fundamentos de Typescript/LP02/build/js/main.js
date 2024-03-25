47893cd0617d960f6e6f5f1d4d16d38c3d7af7d3
Brenda Moura <brendamoura.bcdm@gmail.com>
2024-03-25 19:18:11 -0400

    LP02: edit and delete student

--- Fundamentos de Typescript/LP02/build/js/main.js
+++ Fundamentos de Typescript/LP02/build/js/main.js
@@ -1,37 +1,153 @@
-import Aluno from "./Aluno";
-import Turma from "./Turma";
+"use strict";
+class Aluno {
+    constructor(nomeCompleto, idade, altura, peso) {
+        this.nomeCompleto = nomeCompleto;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.id = ++Aluno.count;
+        this.nomeCompleto = nomeCompleto;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
+Aluno.count = 0;
+class Turma {
+    constructor(nome, listaAlunos) {
+        this.nome = nome;
+        this.listaAlunos = listaAlunos;
+        this.getMediaIdades = () => {
+            const qtdAlunos = this.listaAlunos.length;
+            if (qtdAlunos === 0) {
+                return 0;
+            }
+            const somaIdades = this.listaAlunos.reduce((soma, aluno) => soma + aluno.idade, 0);
+            const media = (somaIdades / qtdAlunos).toFixed(2);
+            return Number(media);
+        };
+        this.getMediaAlturas = () => {
+            const qtdAlunos = this.listaAlunos.length;
+            if (qtdAlunos === 0) {
+                return 0;
+            }
+            const somaAlturas = this.listaAlunos.reduce((soma, aluno) => soma + aluno.altura, 0);
+            const media = (somaAlturas / qtdAlunos).toFixed(2);
+            return Number(media);
+        };
+        this.getMediaPesos = () => {
+            const qtdAlunos = this.listaAlunos.length;
+            if (qtdAlunos === 0) {
+                return 0;
+            }
+            const somaPesos = this.listaAlunos.reduce((soma, aluno) => soma + aluno.peso, 0);
+            const media = (somaPesos / qtdAlunos).toFixed(2);
+            return Number(media);
+        };
+        this.id = ++Turma.count;
+        this.nome = nome;
+        this.listaAlunos = listaAlunos;
+    }
+    getNumAlunos() {
+        return this.listaAlunos.length;
+    }
+}
+Turma.count = 0;
 const btnAdd = document.getElementById("add");
 const list = document.getElementById("students");
+// MODAL
 const modal = document.getElementById("modal");
+const id = document.getElementById("idStudent");
+const fullName = document.getElementById("name");
+const age = document.getElementById("age");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
 const closeModal = document.getElementsByClassName("close")[0];
 const btnSave = document.getElementById("save");
-const turma = new Turma(1, "Educação Física", []);
+// METRICS
+const metrics = document.getElementById("metrics").children;
+// CLASS
+const turma = new Turma("Educação Física", []);
 btnAdd.addEventListener("click", () => {
     modal.style.display = "block";
 });
 closeModal.addEventListener("click", () => {
     modal.style.display = "none";
+    clearModal();
 });
 window.addEventListener("click", (e) => {
     if (e.target == modal) {
         modal.style.display = "none";
+        clearModal();
     }
 });
 btnSave.addEventListener("click", () => {
-    const name = document.getElementById("name").value;
-    const age = Number(document.getElementById("age").value);
-    const height = Number(document.getElementById("height").value);
-    const weight = Number(document.getElementById("weight").value);
-    if (!name || !age || !height || !weight) {
+    if (!fullName.value || !age.value || !height.value || !weight.value) {
         alert("Preencha todos os campos");
         return;
     }
-    const index = turma.getNumAlunos() + 1;
-    const student = new Aluno(index, name, age, height, weight);
-    turma.listaAlunos.push(student);
-    addStudent(student, index);
+    if (!id.value) {
+        const student = new Aluno(fullName.value, Number(age.value), Number(height.value), Number(weight.value));
+        turma.listaAlunos.push(student);
+        addStudent(student);
+        clearModal();
+        loadMetrics();
+    }
+    else {
+        const index = turma.listaAlunos.findIndex((s) => s.id === Number(id.value));
+        const student = {
+            id: Number(id.value),
+            nomeCompleto: fullName.value,
+            idade: Number(age.value),
+            altura: Number(height.value),
+            peso: Number(weight.value),
+        };
+        turma.listaAlunos[index] = student;
+        editStudent(student);
+        clearModal();
+        loadMetrics();
+    }
 });
-const addStudent = (student, index) => {
+const clearModal = () => {
+    id.value = "";
+    fullName.value = "";
+    age.value = "";
+    height.value = "";
+    weight.value = "";
+};
+const loadModal = (idElement) => {
+    const student = turma.listaAlunos.find((s) => s.id === idElement);
+    id.value = student.id.toString();
+    fullName.value = student.nomeCompleto;
+    age.value = student.idade.toString();
+    height.value = student.altura.toString();
+    weight.value = student.peso.toString();
+};
+const loadMetrics = () => {
+    metrics[0].querySelector("h1").textContent = turma.getNumAlunos().toString();
+    metrics[1].querySelector("h1").textContent = turma
+        .getMediaIdades()
+        .toString();
+    metrics[2].querySelector("h1").textContent = turma
+        .getMediaAlturas()
+        .toString();
+    metrics[3].querySelector("h1").textContent = turma
+        .getMediaPesos()
+        .toString();
+};
+const deleteStudent = (idElement) => {
+    const item = document.getElementById(`item${idElement}`);
+    list.removeChild(item);
+    turma.listaAlunos = turma.listaAlunos.filter((s) => s.id !== idElement);
+    loadMetrics();
+};
+const editStudent = (student) => {
+    const item = document.getElementById(`item${student.id}`);
+    item.querySelector("h2").textContent = student.nomeCompleto;
+    item.querySelector("p").textContent = student.idade.toString();
+    modal.style.display = "none";
+};
+const addStudent = (student) => {
     const card = document.createElement("div");
     card.classList.add("card-student");
     const content = document.createElement("div");
@@ -39,20 +155,21 @@ const addStudent = (student, index) => {
     const title = document.createElement("h2");
     title.textContent = student.nomeCompleto;
     const age = document.createElement("p");
-    age.textContent = student.idade.toString();
+    age.textContent = `${student.idade} anos`;
     const action = document.createElement("div");
     action.classList.add("action");
     const btnEdit = document.createElement("input");
     btnEdit.setAttribute("type", "button");
     btnEdit.value = "Editar";
     btnEdit.addEventListener("click", () => {
-        //
+        loadModal(student.id);
+        modal.style.display = "block";
     });
     const btnDelete = document.createElement("input");
     btnDelete.setAttribute("type", "button");
     btnDelete.value = "Apagar";
     btnDelete.addEventListener("click", () => {
-        //
+        deleteStudent(student.id);
     });
     content.appendChild(title);
     content.appendChild(age);
@@ -60,6 +177,8 @@ const addStudent = (student, index) => {
     action.appendChild(btnDelete);
     card.appendChild(content);
     card.appendChild(action);
-    card.id = `item${index}`;
+    card.id = `item${student.id}`;
     list.appendChild(card);
+    modal.style.display = "none";
 };
+loadMetrics();
--- Fundamentos de Typescript/LP02/src/main.ts
+++ Fundamentos de Typescript/LP02/src/main.ts
@@ -1,12 +1,116 @@
-import Aluno from "./Aluno";
-import Turma from "./Turma";
+type averageFunction = () => number;
+
+class Aluno {
+  static count: number = 0;
+  public readonly id: number;
+  constructor(
+    public nomeCompleto: string,
+    public idade: number,
+    public altura: number,
+    public peso: number
+  ) {
+    this.id = ++Aluno.count;
+    this.nomeCompleto = nomeCompleto;
+    this.idade = idade;
+    this.altura = altura;
+    this.peso = peso;
+  }
+}
+
+class Turma {
+  static count: number = 0;
+  public readonly id: number;
+
+  constructor(public nome: string, public listaAlunos: Aluno[]) {
+    this.id = ++Turma.count;
+    this.nome = nome;
+    this.listaAlunos = listaAlunos;
+  }
+
+  public getNumAlunos(): number {
+    return this.listaAlunos.length;
+  }
+
+  public getMediaIdades: averageFunction = () => {
+    const qtdAlunos: number = this.listaAlunos.length;
+
+    if (qtdAlunos === 0) {
+      return 0;
+    }
+
+    const somaIdades: number = this.listaAlunos.reduce(
+      (soma: number, aluno: Aluno) => soma + aluno.idade,
+      0
+    );
+
+    const media = (somaIdades / qtdAlunos).toFixed(2);
+
+    return Number(media);
+  };
+
+  public getMediaAlturas: averageFunction = () => {
+    const qtdAlunos: number = this.listaAlunos.length;
+
+    if (qtdAlunos === 0) {
+      return 0;
+    }
+
+    const somaAlturas: number = this.listaAlunos.reduce(
+      (soma: number, aluno: Aluno) => soma + aluno.altura,
+      0
+    );
+
+    const media = (somaAlturas / qtdAlunos).toFixed(2);
+
+    return Number(media);
+  };
+
+  public getMediaPesos: averageFunction = () => {
+    const qtdAlunos: number = this.listaAlunos.length;
+
+    if (qtdAlunos === 0) {
+      return 0;
+    }
+
+    const somaPesos: number = this.listaAlunos.reduce(
+      (soma: number, aluno: Aluno) => soma + aluno.peso,
+      0
+    );
+
+    const media = (somaPesos / qtdAlunos).toFixed(2);
+
+    return Number(media);
+  };
+}
+
 const btnAdd: HTMLElement = document.getElementById("add")!;
 const list: HTMLElement = document.getElementById("students")!;
+
+// MODAL
 const modal: HTMLElement = document.getElementById("modal")!;
+const id: HTMLInputElement = document.getElementById(
+  "idStudent"
+)! as HTMLInputElement;
+const fullName: HTMLInputElement = document.getElementById(
+  "name"
+)! as HTMLInputElement;
+const age: HTMLInputElement = document.getElementById(
+  "age"
+)! as HTMLInputElement;
+const height: HTMLInputElement = document.getElementById(
+  "height"
+)! as HTMLInputElement;
+const weight: HTMLInputElement = document.getElementById(
+  "weight"
+)! as HTMLInputElement;
 const closeModal = document.getElementsByClassName("close")[0]!;
 const btnSave = document.getElementById("save")!;
 
-const turma = new Turma(1, "Educação Física", []);
+// METRICS
+const metrics: HTMLCollection = document.getElementById("metrics")!.children;
+
+// CLASS
+const turma = new Turma("Educação Física", []);
 
 btnAdd.addEventListener("click", () => {
   modal.style.display = "block";
@@ -14,41 +118,100 @@ btnAdd.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
+  clearModal();
 });
 
 window.addEventListener("click", (e) => {
   if (e.target == modal) {
     modal.style.display = "none";
+    clearModal();
   }
 });
 
 btnSave.addEventListener("click", () => {
-  const name: string | null = (
-    document.getElementById("name")! as HTMLInputElement
-  ).value;
-  const age: number | null = Number(
-    (document.getElementById("age")! as HTMLInputElement).value
-  );
-  const height: number | null = Number(
-    (document.getElementById("height")! as HTMLInputElement).value
-  );
-  const weight: number | null = Number(
-    (document.getElementById("weight")! as HTMLInputElement).value
-  );
-
-  if (!name || !age || !height || !weight) {
+  if (!fullName.value || !age.value || !height.value || !weight.value) {
     alert("Preencha todos os campos");
     return;
   }
-  const index: number = turma.getNumAlunos() + 1;
-  const student: Aluno = new Aluno(index, name, age, height, weight);
 
-  turma.listaAlunos.push(student);
+  if (!id.value) {
+    const student: Aluno = new Aluno(
+      fullName.value,
+      Number(age.value),
+      Number(height.value),
+      Number(weight.value)
+    );
 
-  addStudent(student, index);
+    turma.listaAlunos.push(student);
+    addStudent(student);
+    clearModal();
+    loadMetrics();
+  } else {
+    const index: number = turma.listaAlunos.findIndex(
+      (s) => s.id === Number(id.value)
+    );
+
+    const student: Aluno = {
+      id: Number(id.value),
+      nomeCompleto: fullName.value,
+      idade: Number(age.value),
+      altura: Number(height.value),
+      peso: Number(weight.value),
+    };
+    turma.listaAlunos[index] = student;
+
+    editStudent(student);
+    clearModal();
+    loadMetrics();
+  }
 });
 
-const addStudent = (student: Aluno, index: number): void => {
+const clearModal = () => {
+  id.value = "";
+  fullName.value = "";
+  age.value = "";
+  height.value = "";
+  weight.value = "";
+};
+
+const loadModal = (idElement: number) => {
+  const student: Aluno = turma.listaAlunos.find((s) => s.id === idElement)!;
+
+  id.value = student.id.toString();
+  fullName.value = student.nomeCompleto;
+  age.value = student.idade.toString();
+  height.value = student.altura.toString();
+  weight.value = student.peso.toString();
+};
+
+const loadMetrics = () => {
+  metrics[0].querySelector("h1")!.textContent = turma.getNumAlunos().toString();
+  metrics[1].querySelector("h1")!.textContent = turma
+    .getMediaIdades()
+    .toString();
+  metrics[2].querySelector("h1")!.textContent = turma
+    .getMediaAlturas()
+    .toString();
+  metrics[3].querySelector("h1")!.textContent = turma
+    .getMediaPesos()
+    .toString();
+};
+
+const deleteStudent = (idElement: number): void => {
+  const item: HTMLElement = document.getElementById(`item${idElement}`)!;
+  list.removeChild(item);
+  turma.listaAlunos = turma.listaAlunos.filter((s) => s.id !== idElement);
+  loadMetrics();
+};
+
+const editStudent = (student: Aluno): void => {
+  const item: HTMLElement = document.getElementById(`item${student.id}`)!;
+  item.querySelector("h2")!.textContent = student.nomeCompleto;
+  item.querySelector("p")!.textContent = student.idade.toString();
+  modal.style.display = "none";
+};
+
+const addStudent = (student: Aluno): void => {
   const card: HTMLDivElement = document.createElement("div");
   card.classList.add("card-student");
 
@@ -59,7 +222,7 @@ const addStudent = (student: Aluno, index: number): void => {
   title.textContent = student.nomeCompleto;
 
   const age: HTMLParagraphElement = document.createElement("p");
-  age.textContent = student.idade.toString();
+  age.textContent = `${student.idade} anos`;
 
   const action: HTMLDivElement = document.createElement("div");
   action.classList.add("action");
@@ -68,14 +231,15 @@ const addStudent = (student: Aluno, index: number): void => {
   btnEdit.setAttribute("type", "button");
   btnEdit.value = "Editar";
   btnEdit.addEventListener("click", () => {
-    //
+    loadModal(student.id);
+    modal.style.display = "block";
   });
 
   const btnDelete: HTMLInputElement = document.createElement("input");
   btnDelete.setAttribute("type", "button");
   btnDelete.value = "Apagar";
   btnDelete.addEventListener("click", () => {
-    //
+    deleteStudent(student.id);
   });
 
   content.appendChild(title);
@@ -86,7 +250,10 @@ const addStudent = (student: Aluno, index: number): void => {
 
   card.appendChild(content);
   card.appendChild(action);
-  card.id = `item${index}`;
+  card.id = `item${student.id}`;
 
   list.appendChild(card);
+  modal.style.display = "none";
 };
+
+loadMetrics();
