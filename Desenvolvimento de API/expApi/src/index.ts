915ee0c91c7e3d8406836b531a19522bcf4e8753
Brenda Moura <brenda.moura@icomp.ufam.edu.br>
2024-04-16 20:55:13 -0400

    Desenvolvimento de API: primeiro commit

---
+++ Desenvolvimento de API/expApi/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import validateEnv from "./utils/validateEnv";
+import router from "./router";
+
+dotenv.config();
+validateEnv();
+
+const app = express();
+const PORT = process.env.PORT ?? 4444;
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on the port ${PORT}`);
+});
